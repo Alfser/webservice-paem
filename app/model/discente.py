295521972e49f889650e95af80a36839a3a2fd14
alfser <j.janilson12@gmail.com>
2021-09-24 10:25:00 -0300

    [feat] Adding new features on discente table

--- app/model/discente.py
+++ app/model/discente.py
@@ -13,13 +13,17 @@ class DiscenteModel(BaseHasUsuarioModel, db.Model):
     matricula = db.Column(db.String(45), unique=True, nullable=False)
     nome = db.Column(db.String(45), nullable=False)
     entrada = db.Column(db.String(6), nullable=True)
+    data_nascimento = db.Column(db.String(45), nullable=True)
+    sexo = db.Column(db.String(2), nullable=True)
+    quantidade_pessoas = db.Column(db.Integer, nullable=True)
+    quantidade_vacinas = db.Column(db.Integer, nullable=True)
+    fabricante = db.Column(db.String(45), nullable=True)
+    justificativa = db.Column(db.Text, nullable=True)
     semestre = db.Column(db.Integer, nullable=True)
     endereco = db.Column(db.String(45), nullable=True)
     grupo_risco = db.Column(db.SmallInteger, nullable=True)
     status_covid = db.Column(db.SmallInteger, nullable=True)
-    status_permissao = db.Column(db.SmallInteger, nullable=True) # if is covid or not
-
-    # TODO: add status_alerta
+    status_permissao = db.Column(db.SmallInteger, nullable=True)
 
     usuario_id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), nullable=True)
     usuario = db.relationship('UsuarioModel', uselist=False, lazy='select')
@@ -53,15 +57,21 @@ class DiscenteModel(BaseHasUsuarioModel, db.Model):
                 "id_discente": self.id_discente, 
                 "nome": self.nome,
                 "matricula": self.matricula,
-                "entrada":self.entrada,
-                "semestre":self.semestre,
-                "endereco":self.endereco,
-                "grupo_risco":self.grupo_risco,
-                "status_covid":self.status_covid,
-                "status_permissao":self.status_permissao,
+                "entrada": self.entrada,
+                "data_nascimento": self.data_nascimento,
+                "sexo": self.sexo,
+                "quantidade_pessoas": self.quantidade_pessoas,
+                "quantidade_vacinas": self.quantidade_vacinas,
+                "fabricantes": self.fabricante,
+                "justificativa": self.justificativa,
+                "semestre": self.semestre,
+                "endereco": self.endereco,
+                "grupo_risco": self.grupo_risco,
+                "status_covid": self.status_covid,
+                "status_permissao": self.status_permissao,
                 "usuario": usuario_dict if usuario_dict else "null",
                 "curso": curso.nome if curso else "null",
-                "campus_id_campus":self.campus_id_campus,
+                "campus_id_campus": self.campus_id_campus,
                 "campus": campus.nome if campus else "null"
             }
     
