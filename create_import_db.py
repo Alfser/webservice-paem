cc6bf6954b19d04bfc14718d9dbbfb2a1dde706e
alfser <j.janilson12@gmail.com>
2021-10-16 09:54:05 -0300

    change script create_import database to read separated column by ";"

--- create_import_db.py
+++ create_import_db.py
@@ -35,17 +35,17 @@ def dicts2db(dicts, Model):
 
 def import_csv_db():
 
-    campus_dicts = read_csv('app/database/inputs/campus_instituto.csv', parse_dates=["ano_fundacao"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    curso_dicts = read_csv('app/database/inputs/curso.csv', parse_dates=["data_fundacao"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    discente_dicts = read_csv('app/database/inputs/discente.csv', na_filter=True, sep=',', encoding='UTF-8').replace(np.nan, None).replace([np.nan], [None]).to_dict(orient='records')
-    docente_dicts = read_csv('app/database/inputs/docente.csv', parse_dates=["data_nascimento"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    recurso_campus_dicts = read_csv('app/database/inputs/recurso_campus.csv', parse_dates=["inicio_horario_funcionamento", "fim_horario_funcionamento"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    tecnico_dicts = read_csv('app/database/inputs/tecnico.csv', parse_dates=["data_nascimento"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    usuario_dicts = read_csv('app/database/inputs/usuario.csv', na_filter=True, sep=',', encoding='UTF-8', keep_default_na=False).replace([np.nan], [None]).to_dict(orient='records')
-    solicitacao_acesso_dicts = read_csv('app/database/inputs/solicitacao_acesso.csv', parse_dates=["data", "hora_inicio", "hora_fim"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    acesso_permitido_dicts = read_csv('app/database/inputs/acesso_permitido.csv', parse_dates=["hora_entrada", "hora_saida"], na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    protocolo_dicts = read_csv('app/database/inputs/protocolo.csv', na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
-    disciplinas_dicts = read_csv('app/database/inputs/disciplina.csv', na_filter=True, sep=',', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    campus_dicts = read_csv('app/database/inputs/campus_instituto.csv', parse_dates=["ano_fundacao"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    curso_dicts = read_csv('app/database/inputs/curso.csv', parse_dates=["data_fundacao"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    discente_dicts = read_csv('app/database/inputs/discente.csv', na_filter=True, sep=';', encoding='UTF-8').replace(np.nan, None).replace([np.nan], [None]).to_dict(orient='records')
+    docente_dicts = read_csv('app/database/inputs/docente.csv', parse_dates=["data_nascimento"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    recurso_campus_dicts = read_csv('app/database/inputs/recurso_campus.csv', parse_dates=["inicio_horario_funcionamento", "fim_horario_funcionamento"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    tecnico_dicts = read_csv('app/database/inputs/tecnico.csv', parse_dates=["data_nascimento"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    usuario_dicts = read_csv('app/database/inputs/usuario.csv', na_filter=True, sep=';', encoding='UTF-8', keep_default_na=False).replace([np.nan], [None]).to_dict(orient='records')
+    solicitacao_acesso_dicts = read_csv('app/database/inputs/solicitacao_acesso.csv', parse_dates=["data", "hora_inicio", "hora_fim"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    acesso_permitido_dicts = read_csv('app/database/inputs/acesso_permitido.csv', parse_dates=["hora_entrada", "hora_saida"], na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    protocolo_dicts = read_csv('app/database/inputs/protocolo.csv', na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
+    disciplinas_dicts = read_csv('app/database/inputs/disciplina.csv', na_filter=True, sep=';', encoding='UTF-8').replace([np.nan], [None]).to_dict(orient='records')
 
     # print(campus_dicts)
     
