ca018e98a04c62fc2da391ba9c835fb697dc17ff
alfser <j.janilson12@gmail.com>
2021-12-04 00:48:35 -0300

    [alteration] change carteirinha_vacinacao from Text to String.
    Problem: just to allow send pdf file from base64 method
    
    Implementation: it was made by the alteration in discente.py, docente.py and tecnico.py files.

--- app/model/discente.py
+++ app/model/discente.py
@@ -14,7 +14,7 @@ class DiscenteModel(BaseHasUsuarioModel, db.Model):
     matricula = db.Column(db.String(45), unique=True, nullable=False)
     nome = db.Column(db.String(255), nullable=False)
     entrada = db.Column(db.String(6), nullable=True)
-    carteirinha_vacinacao = db.Column(db.Text, nullable=True)
+    carteirinha_vacinacao = db.Column(db.String, nullable=True)
     __data_nascimento = db.Column('data_nascimento', db.Date, nullable=True)
     ano_de_ingresso = db.Column(db.Integer, nullable=True)
     sexo = db.Column(db.String(2), nullable=True)
